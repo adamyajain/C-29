5a9524e9ff5ed0346c49ac3303f74b2a24caae1e
adamyajain <apoorvadoc@gmail.com>
2020-03-19 19:14:28 +0530

    commited

--- Slingshot.js
+++ Slingshot.js
@@ -17,27 +17,29 @@ class SlingShot{
     fly(){
         this.sling.bodyA = null;
     }
-
+    attach(body){
+       this.sling.bodyA = body; 
+    }
     display(){
-        image(this.sling1,200,20);
-        image(this.sling2,175,20);
+        image(this.sling1,175,70);
+        image(this.sling2,150,70);
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            if(pointA.x<220){
+            if(pointA.x<510){
                 strokeWeight(10);
                 stroke(48,22,8);
-                line(pointA.x-25, pointA.y, pointB.x-10, pointB.y);
-                line(pointA.x-25, pointA.y, pointB.x+20, pointB.y);
+                line(pointA.x-50, pointA.y, pointB.x-35, pointB.y);
+                line(pointA.x-50, pointA.y, pointB.x-5, pointB.y);
                 image(this.sling3,pointA.x-30,pointA.y-15,10,40);
             }
-            else{
+/*            else{
                 strokeWeight(4);
                 stroke(48,22,8);
-                line(pointA.x-25, pointA.y, pointB.x-10, pointB.y);
-                line(pointA.x-25, pointA.y, pointB.x+20, pointB.y);
+                line(pointA.x-50, pointA.y, pointB.x-35, pointB.y);
+                line(pointA.x-50, pointA.y, pointB.x-50, pointB.y);
                 image(this.sling3,pointA.x-30,pointA.y-15,10,40)  
-            }
+            }*/
         }
     }
     
